28681605311f834c50cb2e88d5006ddc4b4f7f18
GABRIEL FONSECA <gabrieldfnsc@gmail.com>
2020-07-10 15:43:08 -0300

    final touch

--- resources/js/components/DateResult/Item.vue
+++ resources/js/components/DateResult/Item.vue
@@ -2,12 +2,22 @@
     <li
         class="list-group-item text-center"
         v-html="
-            `${item.start_date} - ${item.end_date} = <span class='badge badge-success badge-pill'>${item.result}</span>`
+            `${startDate} - ${endDate} = <span class='badge badge-success badge-pill'>${
+                item.result
+            }</span> ${item.result == 1 ? 'day' : 'days'}`
         "
     ></li>
 </template>
 <script>
 export default {
     props: ["item"],
+    computed: {
+        startDate() {
+            return moment(this.item.start_date).format("DD/MM/YYYY");
+        },
+        endDate() {
+            return moment(this.item.end_date).format("DD/MM/YYYY");
+        },
+    },
 };
 </script>
